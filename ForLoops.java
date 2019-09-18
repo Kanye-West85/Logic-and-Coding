cef6d4741444d214da287e13015a703747b56dc9
Kanye-West85 <54414557+Kanye-West85@users.noreply.github.com>
2019-09-18 11:12:10 -0700

    Add files via upload

---
+++ ForLoops.java
@@ -0,0 +1,26 @@
+/**
+ * part a
+ * cubes of intergers from 1 to 10 inclusive
+ */
+public class ForLoops
+{ 
+    public static void main ( String args [] ) {
+       int x;
+       int cube = 1;
+       int y;
+       int oddint = 1;
+       
+        for (x = 1; x <= 10; x = x + 1) {
+            cube = x * x * x;
+            System.out.println(cube);
+        }
+        for (y = 101; y >= 3; y = y - 2) {
+            oddint = y - 2;
+            System.out.println(oddint);
+        }
+        for (public static void main  (String args[] ); ) {
+            n = myStr.length (); 
+            n = 36;
+        
+        }
+    }
